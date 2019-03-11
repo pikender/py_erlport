c58991705c6196f331d9ea345de84cea886c9dfa
Pikender Sharma <avid.piks@gmail.com>
2019-03-11 15:50:41 +0530

    Init

---
+++ lib/datagen_erlport.ex
@@ -0,0 +1,18 @@
+defmodule DatagenErlport do
+  @moduledoc """
+  Documentation for DatagenErlport.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DatagenErlport.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/datagen_erlport_test.exs
@@ -0,0 +1,8 @@
+defmodule DatagenErlportTest do
+  use ExUnit.Case
+  doctest DatagenErlport
+
+  test "greets the world" do
+    assert DatagenErlport.hello() == :world
+  end
+end
