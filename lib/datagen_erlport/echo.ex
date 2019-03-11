095b61291cced64b13c6d44b05d99020c112d669
Pikender Sharma <avid.piks@gmail.com>
2019-03-11 18:40:50 +0530

    Newline at end and only one

--- lib/datagen_erlport/echo.ex
+++ lib/datagen_erlport/echo.ex
@@ -2,15 +2,34 @@ defmodule DatagenErlport.Echo do
   alias DatagenErlport.Errors.{InvalidStdinNewline}
 
   def echo(msg) do
+    msg
+    |> validate_eol()
+    |> IO.inspect(label: "Eol")
+    |> validate_only_one_eol(?\n)
+    |> IO.inspect(label: "Only Eol")
+  end
+
+  def validate_eol(msg) do
     case is_newline_terminated(to_charlist(msg)) do
       true -> msg
       false -> raise(InvalidStdinNewline)
     end
   end
 
+  def validate_only_one_eol(msg, char) do
+    case count_chars(to_charlist(msg), char) do
+      1 -> msg
+      _ -> raise(InvalidStdinNewline)
+    end
+  end
+
   defp is_newline_terminated([]), do: false
   defp is_newline_terminated('\n'), do: true
   defp is_newline_terminated([_|t]), do: is_newline_terminated(t)
+
+  def count_chars(str, char) do
+    length(for x <- to_charlist(str), x == char, do: x)
+  end
 end
 
 defmodule DatagenErlport.Errors.InvalidStdinNewline do
--- test/datagen_erlport_test.exs
+++ test/datagen_erlport_test.exs
@@ -3,7 +3,9 @@ defmodule DatagenErlportTest do
 
   alias DatagenErlport.{Echo, Errors}
 
+  @invalid_message_bw "m\ne"
   @invalid_message "me"
+  @invalid_message_m "me \n\n"
   @valid_message "me \n"
 
   describe "message to python stdin" do
@@ -14,6 +16,22 @@ defmodule DatagenErlportTest do
     test "not terminated by newline raises exception" do
       assert_raise Errors.InvalidStdinNewline, fn ->
         Echo.echo(@invalid_message)
+      end
+    end
+
+    test "not terminated by newline but having in b/w raises exception" do
+      assert_raise Errors.InvalidStdinNewline, fn ->
+        Echo.echo(@invalid_message_bw)
+      end
+    end
+
+    test "only one newline in message" do
+      assert @valid_message == Echo.echo(@valid_message)
+    end
+
+    test "more than one newline in message" do
+      assert_raise Errors.InvalidStdinNewline, fn ->
+        Echo.echo(@invalid_message_m)
       end
     end
   end
